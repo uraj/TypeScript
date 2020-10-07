c1ff799308e2c17f027299edf98ca742e2412d2e
Sheetal Nandi <shkamat@microsoft.com>
2020-10-06 21:07:46 -0700

    Use ambientTNameToUnmodifiedFileNames if its program with safe modules

--- src/compiler/program.ts
+++ src/compiler/program.ts
@@ -1465,11 +1465,10 @@ namespace ts {
                 newSourceFiles.push(newSourceFile);
             }
 
+            ambientModuleNameToUnmodifiedFileName = ambientModuleNameToUnmodifiedFileNameUsingOldProgram;
             if (structureIsReused !== StructureIsReused.Completely) {
                 return structureIsReused;
             }
-
-            ambientModuleNameToUnmodifiedFileName = ambientModuleNameToUnmodifiedFileNameUsingOldProgram;
 
             // try to verify results of module resolution
             for (const newSourceFile of modifiedSourceFiles || emptyArray) {
