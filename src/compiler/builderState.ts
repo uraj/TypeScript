c05d16bc8c1a267119592229977920ec3c9c6cc0
Sheetal Nandi <shkamat@microsoft.com>
2020-10-06 18:16:45 -0700

    Let program store resolutions with failed lookup locations

--- src/compiler/builderState.ts
+++ src/compiler/builderState.ts
@@ -141,7 +141,7 @@ namespace ts {
             // Handle type reference directives
             const resolvedTypeReferenceDirectiveNames = program.getPerFileTypeReferenceResolutions().get(sourceFile.path);
             if (resolvedTypeReferenceDirectiveNames) {
-                resolvedTypeReferenceDirectiveNames.forEach((resolvedTypeReferenceDirective) => {
+                resolvedTypeReferenceDirectiveNames.forEach(({ resolvedTypeReferenceDirective }) => {
                     if (!resolvedTypeReferenceDirective) {
                         return;
                     }
