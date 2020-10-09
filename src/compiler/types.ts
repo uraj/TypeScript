ac107c91b18c0a8abf743dc532f20e49505b7a4a
Sheetal Nandi <shkamat@microsoft.com>
2020-10-09 11:31:58 -0700

    Skeleton for buildinfo program

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -6060,7 +6060,7 @@ namespace ts {
     }
 
     /* @internal */
-    export type CreateProgramOptionsWithProgramFromBuildInfo = Omit<CreateProgramOptions, "oldProgram"> & { oldProgram: ProgramFromBuildInfo; };
+    export type CreateProgramOptionsWithProgramFromBuildInfo = Omit<CreateProgramOptions, "oldProgram"> & { oldProgram: ProgramFromBuildInfo | Program | undefined; };
 
     /* @internal */
     export interface CommandLineOptionBase {
