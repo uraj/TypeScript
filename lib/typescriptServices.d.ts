d9a6ad6035aed7a9a08c0b10f0627096fb5447d2
Pei Wang <uraj.wp@gmail.com>
2020-10-12 19:44:04 -0700

    Make executeCommandLine a public API.

--- lib/typescriptServices.d.ts
+++ lib/typescriptServices.d.ts
@@ -7114,3 +7114,8 @@ declare namespace ts {
     interface Map<T> extends ESMap<string, T> {
     }
 }
+declare namespace ts {
+    function isBuild(commandLineArgs: readonly string[]): boolean;
+    type ExecuteCommandLineCallbacks = (program: Program | EmitAndSemanticDiagnosticsBuilderProgram | ParsedCommandLine) => void;
+    function executeCommandLine(system: System, cb: ExecuteCommandLineCallbacks, commandLineArgs: readonly string[]): void | WatchOfConfigFile<EmitAndSemanticDiagnosticsBuilderProgram> | SolutionBuilder<EmitAndSemanticDiagnosticsBuilderProgram>;
+}
